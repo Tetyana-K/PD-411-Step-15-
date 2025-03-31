9b9d1e61af17c68f62dc66c4285266bcf06b152a
Tetyana-K <tanya.kyryk@gmail.com>
2025-03-31 20:37:36 +0300

    add preprocessor project

--- pointer to fun/pointer to fun as param.cpp
+++ pointer to fun/pointer to fun as param.cpp
@@ -1,6 +1,8 @@
 ﻿// pointer to fun.cpp : This file contains the 'main' function. Program execution begins and ends there.
 //
 
+#define DEBUG 
+
 #include <iostream>
 #include <cctype>
 using namespace std;
@@ -36,15 +38,19 @@ int countByCriteria(const int arr[], int size, bool criteria(int))
 	{
 		if (criteria(arr[i])) // якщо поточний елемент масиву задовільняє критерію, який переданий у функцію
 		{
+#ifdef DEBUG // препроцесорне розгалуження
 			cout << "\t" << arr[i];
+#endif
 			++counter;
 		}
 	}
+#ifdef DEBUG
 	cout << endl;
+#endif
 	return counter;
 }
 
-bool isNegative(int value) //цю функцію потім використаємо як критерій для ф.countByCriteria(arr,7, isNegative)
+bool isNegative(int value) //цю функцію потім використаємо як критерій для функції countByCriteria(arr,7, isNegative)
 {
 	return value < 0;
 }
@@ -75,3 +81,7 @@ int main()
 }
 
 // 1. Визначити функції обчислення суми, різниці, добутку, частки двох дробових чисел. Викликати функції через вказівник функції.
+// 2. Організувати меню за допомогою масиву вказівників на функції  обчислення суми, різниці, добутку, частки двох дробових чисел. 
+// 3. Визначити  функцію arraysAction(масив1, масив2, масив3, функціяДія), яка приймає як аргументи покажчики на три масиви однакові за розміром,  їх розмір та вказівник на функцію. Функція arraysAction() повинна виконати операцію, передану через вказіваник на функцію над відповідними елементами перших двох масивів,та занести результат у відповіний елемент треього масиву.
+// Створити функції для виконання дій (наприклад додавання, віднімання, множення, середнЄ, максимальне з двох).
+
---
+++ preprocessor C/preprocessor C.cpp
@@ -0,0 +1,55 @@
+﻿// preprocessor C.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+// Препроцесор - програма, яка працює ДО початку компіляції сpp-програми, виконує ТЕКСТОВІ заміни у коді. 
+
+#include <iostream> // препроцесор вставить вміст файлу у цю точку
+using namespace std;
+
+
+#define SIZE 5 // визначили препроцесорну константу, препроцесор далі по коду позамінює SIZE на 5
+
+#define MAX(a,b) ((a)> (b)? (a): (b)) // визначили макрос з іменем MAX, макрос має два параметри, у точку виклику макроса препроцесор підставить тіло макросу на фактичних параметрах
+
+//#define INTEGER
+#define DOUBLE
+
+
+#define ROWS 2
+#define COLS 3
+
+int main()
+{
+	cout << "SIZE = " << SIZE << endl; // cout << "SIZE = " << 5 << endl;
+	int arr[SIZE]{ 10, 20, 33, 50, 101 }; // int arr[5]{ 10, 20, 33, 50, 101 }; 
+
+	int num1 = 12, num2 = 30;
+	cout << "MAX = " << MAX(num1, num2) << endl;//  cout << "MAX = " << ((num1)> (num2)? (num1): (num2))  << endl;
+
+#ifdef INTEGER // якщо визначено препроцесорне імя INTEGER, 
+	int matrix[ROWS][COLS]{ 1,2,3,4 }; // то створюється матриця з цілих чисел
+#elif defined(DOUBLE) // інакше, якщо визначено препроцесорне імя DOUBLE,
+	double matrix[ROWS][COLS]{ 1.1,2.2,3.3, 4.4 }; // то створюється матриця з дробових  чисел
+#else          //інакше
+	char matrix[ROWS][COLS]{ 'a','b','c','d' };// то створюється матриця з символів
+#endif
+	// Виводимо на екран матрицю, зараз виведе матрицю дробову, бо є #define DOUBLE (#define INTEGER закоментований)
+	for (int i =0; i < ROWS; ++i)
+	{
+		for (int j =0; j < COLS; ++j)
+		{
+			cout << matrix[i][j] << "\t";
+		}
+		cout << endl;
+	}
+	/*for (auto& row : matrix)
+	{
+		for (auto& elem : row)
+		{
+			cout << elem << "\t";
+		}
+		cout << endl;
+	}*/
+
+}
+
